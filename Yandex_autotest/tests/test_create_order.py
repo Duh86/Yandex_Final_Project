80b03cb543c8c7ef76e9d4638ce8f5ca123f3f61
Peter <petrkutuzov85@gmail.com>
2025-05-13 10:11:02 +0300

    s

---
+++ Yandex_autotest/tests/test_create_order.py
@@ -0,0 +1,23 @@
+import requests
+from config.configuration import BASE_URL, CREATE_ORDER_PATH, GET_ORDER_PATH
+
+def test_create_and_get_order():
+    create_response = requests.post(BASE_URL + CREATE_ORDER_PATH, json={
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2020-06-06",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+    })
+    assert create_response.status_code == 201
+    track = create_response.json().get("track")
+    assert track is not None
+
+    get_response = requests.get(BASE_URL + GET_ORDER_PATH, params={"t":track})
+    assert get_response.status_code == 200
